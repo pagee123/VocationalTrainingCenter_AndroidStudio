c8cd029952cfe8eee77055b8b141c1810caecebb
Daniel <angry871015@gmail.com>
2024-10-07 14:49:17 +0800

    esp8266LedControl

--- HTTP_2/app/src/main/java/com/example/http_2/MainActivity.java
+++ HTTP_2/app/src/main/java/com/example/http_2/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.http_2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +16,45 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WebView webView;
+    private WebSettings webset;
+    private EditText editTextIpAddr;
+    private Button buttonLink;
+    private String ipAddr;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        //EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        webView = (WebView) findViewById(R.id.webView_id);
+        webset = webView.getSettings();
+        webset.setJavaScriptEnabled(true);
+        webset.setSupportZoom(true);
+        webset.setBuiltInZoomControls(true);
+
+        editTextIpAddr = (EditText)findViewById(R.id.editText_ipAddr);
+        buttonLink = (Button) findViewById(R.id.button_link);
+        
+        buttonLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(editTextIpAddr.length()==0){
+                    ipAddr = "https://www.google.com.tw";
+                    editTextIpAddr.setText(ipAddr);
+                }else{
+                    ipAddr = editTextIpAddr.getText().toString();
+                }
+                webView.setWebChromeClient(new WebChromeClient());
+                webView.loadUrl(ipAddr);
+            }
+        });
+
     }
 }
